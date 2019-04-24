0d4b9f26c246ba0329acd79e65c1099e3662f137
DanielRichtersz <digidi-daantje@hotmail.com>
2019-04-24 21:44:30 +0200

    Followed subreddits are loaded

--- RedditClient/src/app/components/timeline/timeline.component.ts
+++ RedditClient/src/app/components/timeline/timeline.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TimelineService } from 'src/app/services/timeline/timeline.service';
+import { Subreddit } from 'src/app/models/Subreddit';
 
 @Component({
   selector: 'app-timeline',
@@ -8,14 +10,36 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TimelineComponent implements OnInit {
 
+  subreddits: Array<Subreddit>;
+  errorMsg: String;
   username: string;
 
   constructor(
     private route: ActivatedRoute,
+    private timelineService: TimelineService,
   ) { }
 
   ngOnInit() {
     this.username = this.route.snapshot.paramMap.get('username');
+    console.log("Current route is: " + this.route);
+    this.getFollowedSubreddits();
+
+
+  }
+
+  getFollowedSubreddits() {
+    this.timelineService.getFollowedSubreddits(this.username).subscribe(fSubreddits => {
+      try {
+        this.errorMsg = "";
+        this.subreddits = fSubreddits;
+
+        console.log("Retrieved subreddits response: ");
+        console.log(fSubreddits);
+      }
+      catch {
+        this.errorMsg = "Something went wrong while loading followed subreddits in, please try again";
+      }
+    })
   }
 
 }
